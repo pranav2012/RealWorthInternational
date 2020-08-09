7ade649f15cbe2b21b8df61d9beba9ebb7bb523f
Pranav Agarwal <p2012agarwal@gmail.com>
2020-08-09 10:25:18 +0530

    favicon icon added

--- src/Components/Slideshow.jsx
+++ src/Components/Slideshow.jsx
@@ -5,11 +5,11 @@ import 'react-slideshow-image/dist/styles.css';
 
 const FadeImage = () => {
   const fadeImages = [
-    require("../assets/images/bag1.jpg"),
-    require("../assets/images/bag2.jpg"),
-    require("../assets/images/bag3.jpg"),
-    require("../assets/images/bag4.jpg"),
-    require("../assets/images/bag5.jpg")
+    "https://us.123rf.com/450wm/gilitukha/gilitukha1801/gilitukha180100094/93808056-traveler-making-coffee-in-cezve-on-a-gas-burner-in-the-mountains-in-turkey.jpg?ver=6",
+    "https://us.123rf.com/450wm/zhukovvvlad/zhukovvvlad1511/zhukovvvlad151100376/48308590-girl-sitting-in-a-sleeping-bag-and-drinking-hot-drink-.jpg?ver=6",
+    "https://us.123rf.com/450wm/shaiith/shaiith1801/shaiith180100170/93514113-view-from-tent-to-sunset-in-mountains-in-summer-poland-.jpg?ver=6",
+    "https://us.123rf.com/450wm/vitalalp/vitalalp1801/vitalalp180100324/93206278-camping-on-the-river-bank-the-solar-panel-hangs-on-the-tent-.jpg?ver=6",
+    "https://us.123rf.com/450wm/pavilion016/pavilion0161802/pavilion016180200005/104951501-new-born-baby-sleeping-in-mosquito-net-baby-in-thailand.jpg?ver=6"
   ];
 
   const properties = {
